18c397181a6230efa77bae431d57c2f8e836e421
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-03 18:55:42 -0500

    fix

--- src/Terminal.js
+++ src/Terminal.js
@@ -2,16 +2,14 @@ import './Terminal.css'
 
 function Terminal(){
     return(
-        <body>
-            <form className = 'Terminal'>
-                <label className='Console'>
-                    Console: 
-                    <input type='text' name='consoleInput' />
-                </label>
-                <br></br>
-                <input type='submit' value="Let's make money..." />
-            </form>
-        </body>
+        <form className = 'Terminal'>
+            <label className='Console'>
+                Console: 
+                <input type='text' name='consoleInput' />
+            </label>
+            <br></br>
+            <input type='submit' value="Let's make money..." />
+        </form>
     )
 }
 

5825f29229a23bdefeefd3aa0bcd056d90108672
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-03 17:22:46 -0500

    Trying React Fragments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
+import React from 'react'
 import PoweredByReact from './PoweredByReact'
 import Terminal from './Terminal'
 import './App.css'
 
 function App(){
   return (
-    <div className="App">
-      <header>
-        <PoweredByReact />
-      </header>
-      <main>
-        <Terminal />
-      </main>
-    </div>
+    <React.Fragment>
+      <PoweredByReact />
+      <Terminal />
+    </React.Fragment>
   );
 }
 

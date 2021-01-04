aec770060e3e4c8a3905a1d994157acc203ba864
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-03 21:45:26 -0500

    coinLayer query implementation

--- src/Terminal.js
+++ src/Terminal.js
@@ -2,13 +2,23 @@ import './Terminal.css'
 
 function Terminal(){
     return(
-        <form className = 'Terminal'>
-            <label className='Console'>
-                Console: 
-                <input type='text' name='consoleInput' />
+        <form className='Terminal'>
+            <label className='coinLayerKeyLabel'>
+                API Key:
+                <input type='text' name='coinLayerKey' />
             </label>
             <br></br>
-            <input type='submit' value="Let's make money..." />
+            <label className='startDateLabel'>
+                Start Date:
+                <input type='text' name='startDate' />
+            </label>
+            <br></br>
+            <label className='endDateLabel'>
+                End Date:
+                <input type='text' name='endDate' />
+            </label>
+            <br></br>
+            <input type='submit' value="Query" />
         </form>
     )
 }

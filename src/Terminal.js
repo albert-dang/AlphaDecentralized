0108d85e64d46f8cd8c13fe91a32b0f99b6a338b
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-03 22:12:51 -0500

    styling

--- src/Terminal.js
+++ src/Terminal.js
@@ -2,24 +2,26 @@ import './Terminal.css'
 
 function Terminal(){
     return(
-        <form className='Terminal'>
-            <label className='CoinLayerKeyLabel'>
-                API Key:
-                <input type='text' name='coinLayerKey' />
-            </label>
-            <br></br>
-            <label className='StartDateLabel'>
-                Start Date:
-                <input type='text' name='startDate' />
-            </label>
-            <br></br>
-            <label className='EndDateLabel'>
-                End Date:
-                <input type='text' name='endDate' />
-            </label>
-            <br></br>
-            <input className='QueryButton' type='submit' value="Query" />
-        </form>
+        <div className='CoinLayerQuery'>
+            <form className='Terminal'>
+                <label className='CoinLayerKeyLabel'>
+                    API Key:
+                    <input type='text' name='coinLayerKey' />
+                </label>
+                <br></br>
+                <label className='StartDateLabel'>
+                    Start Date:
+                    <input type='text' name='startDate' />
+                </label>
+                <br></br>
+                <label className='EndDateLabel'>
+                    End Date:
+                    <input type='text' name='endDate' />
+                </label>
+                <br></br>
+                <input className='QueryButton' type='submit' value="Query" />
+            </form>
+        </div>
     )
 }
 

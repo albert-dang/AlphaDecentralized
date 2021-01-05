71beecbed8de1af8e1a648c473984a794d2a6064
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-04 23:31:09 -0500

    fix

--- src/PoweredByCoinLayer.js
+++ src/PoweredByCoinLayer.js
@@ -3,7 +3,7 @@ import './PoweredByCoinLayer.css'
 
 export default function PoweredByCoinLayer(){
     return(
-        <a href='https://coinlayer.com'>
+        <a href='https://coinlayer.com' target='_blank'>
             <div className='CoinLayer-showcase'>
                 <img src={logo} className='CoinLayer-logo' alt='coinlayer logo'/>
             </div>
--- src/PoweredByReact.js
+++ src/PoweredByReact.js
@@ -3,7 +3,7 @@ import './PoweredByReact.css'
 
 export default function PoweredByReact(){
     return(
-        <a href="https://reactjs.org">
+        <a href="https://reactjs.org" target='_blank'>
             <div className="React-showcase">
                 <img src={logo} className="App-logo" alt="react logo"/>
             </div>

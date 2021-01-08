a23197fba55b32f305006b9c48ec0f5c77af3e59
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-08 04:31:59 -0500

    fix

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,9 +1,26 @@
+import React, {Component} from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import './Report.css'
 
-export default class ReportWrapper{
+export default class ShowcaseSlider extends Component{
     render(){
-        return(
-            <div className='ReportWrapper'></div>
+        const settings = {
+            arrows: true,
+            dots: false,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
+        return (
+            <div className='SliderWrapper'>
+                <Slider {...settings} className='ShowcaseSlider'>
+                    <h2>Stonks 1</h2>
+                    <h2>Stonks 2</h2>
+                    <h2>Stonks 3</h2>
+                </Slider>
+            </div>
         )
     }
 }

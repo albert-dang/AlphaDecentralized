312e11f943128768961fff86c07ae9c1b2bf8c1a
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-08 00:34:19 -0500

    fix

--- src/components/CoinLayerForm.js
+++ src/components/CoinLayerForm.js
@@ -29,25 +29,27 @@ export default class CoinLayerForm extends Component{
     render(){
         const{apiKey, startDate, endDate} = this.state
         return(
-            <form className='QueryForm' onSubmit={this.onSubmit}>
-                <label>Cryptocurrencies</label>
-                <label><a href='https://coinlayer.com/signup/free' rel='noreferrer' target='_blank'>Need a free API Key?</a></label>
-                <label className='CoinLayerKeyLabel'>
-                    API Key:
-                    <input type='text' name='apiKey' value={apiKey} onChange={this.onChange}/>
-                </label>
-                <label className='StartDateLabel'>
-                    Start Date:
-                    <input type='text' name='startDate' value={startDate} onChange={this.onChange}/>
-                </label>
-                <label className='EndDateLabel'>
-                    End Date:
-                    <input type='text' name='endDate' value={endDate} onChange={this.onChange}/>
-                </label>
-                <button className='QueryButton' type='submit'>
-                    Query
-                </button>
-            </form>
+            <div className='QueryFormWrapper'>
+                <form className='QueryForm' onSubmit={this.onSubmit}>
+                    <label>Cryptocurrencies</label>
+                    <label><a href='https://coinlayer.com/signup/free' rel='noreferrer' target='_blank'>Need a free API Key?</a></label>
+                    <label className='CoinLayerKeyLabel'>
+                        API Key:
+                        <input type='text' name='apiKey' value={apiKey} onChange={this.onChange}/>
+                    </label>
+                    <label className='StartDateLabel'>
+                        Start Date:
+                        <input type='text' name='startDate' value={startDate} onChange={this.onChange}/>
+                    </label>
+                    <label className='EndDateLabel'>
+                        End Date:
+                        <input type='text' name='endDate' value={endDate} onChange={this.onChange}/>
+                    </label>
+                    <button className='QueryButton' type='submit'>
+                        Query
+                    </button>
+                </form>
+            </div>
         )
     }
 }

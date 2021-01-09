4da3c21c8ba5b035db79c4c8a2457dd8055c9a1b
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-08 23:41:43 -0500

    fix

--- src/components/CryptoForm.js
+++ src/components/CryptoForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-import './CryptoForm.css'
+import './QueryForm.css'
 
 export default class CryptoForm extends Component{
     constructor(){

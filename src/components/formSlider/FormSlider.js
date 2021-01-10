8ac10744c10fa1bf0dda6526ba41c43e50965769
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-10 17:29:25 -0500

    fix

--- src/components/formSlider/FormSlider.js
+++ src/components/formSlider/FormSlider.js
@@ -13,7 +13,7 @@ export default class FormSlider extends Component{
             adaptiveHeight: true,
             arrows: false,
             centerMode: true,
-            centerPadding: 0,
+            centerPadding: '0%',
             dots: false,
             infinite: true,
             slidesToShow: 1,

f90786d2ca1b259b2f1e7edf9626160e4b9b2dad
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-10 10:51:03 -0500

    fix

--- src/components/formSlider/FormSlider.js
+++ src/components/formSlider/FormSlider.js
@@ -13,6 +13,8 @@ export default class FormSlider extends Component{
             arrows: true,
             dots: false,
             infinite: true,
+            nextArrow: <i class="icon fa-arrow-right nextForm"></i>,
+            prevArrow: <i class="icon fa-arrow-left prevForm"></i>,
             slidesToShow: 1,
             slidesToScroll: 1,
             speed: 785

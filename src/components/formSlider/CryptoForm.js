813321ebaf456b65de7c0aafca47e664303120f7
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-11 16:28:55 -0500

    fix

--- src/components/formSlider/CryptoForm.js
+++ src/components/formSlider/CryptoForm.js
@@ -6,7 +6,6 @@ export default class CryptoForm extends Component{
     constructor(){
         super()
         this.state = {
-            apiKey: 'c4b654682c080684f623cba908b1133a',
             symbol: 'BTC, ETH, XLM'
         }
     }
@@ -17,25 +16,20 @@ export default class CryptoForm extends Component{
 
     onSubmit = (e) => {
         e.preventDefault()
-        const{apiKey, symbol} = this.state
+        const{symbol} = this.state
 
-        axios.post('/', {apiKey, symbol})
+        axios.post('/', {symbol})
             .then((result) => {
                 //Access results
             })
     }
 
     render(){
-        const{apiKey, symbol} = this.state
+        const{symbol} = this.state
         return(
             <div className='QueryFormWrapper'>
                 <form className='QueryForm' onSubmit={this.onSubmit}>
                     <h1>Crypto</h1>
-                    <label><a href='https://coinlayer.com/signup/free' rel='noreferrer' target='_blank'>Need a free API Key?</a></label>
-                    <label className='CoinLayerKeyLabel'>
-                        API Key:
-                        <input type='text' name='apiKey' value={apiKey} onChange={this.onChange}/>
-                    </label>
                     <label className='SymbolLabel'>
                         Symbol(s):
                         <input type='text' name='symbol' value={symbol} onChange={this.onChange}/>

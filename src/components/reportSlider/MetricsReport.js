2bb179c55f3cd4096ec64d74033170c8e042903e
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-14 01:49:50 -0500

    fix

--- src/components/reportSlider/MetricsReport.js
+++ src/components/reportSlider/MetricsReport.js
@@ -1,23 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
-export default function MetricsReport() {
-  const [appState, setAppState] = useState({
-    loading: false,
-    assets: null,
-  });
-
-  useEffect(() => {
-    setAppState({ loading: true });
-    const apiUrl = 'https://dangitsal.pythonanywhere.com/api/fullsheet';
-    axios.get(apiUrl).then((assets) => {
-      const allAssets = assets.data;
-      setAppState({ loading: false, assets: allAssets });
-    });
-  }, [setAppState]);
-  return (
-    <div className='MetricsReport'>
-      <span>${allAssets}</span>
-    </div>
-  )
+export default function MetricsReport(){
+  const data = axios.request({url: 'https://dangitsal.pythonanywhere.com/api/fullsheet'}).data
+  return <p>${data}</p>
 }

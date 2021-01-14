375691bcfa7aae272cb0e79588252745f6d54c19
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-14 00:57:52 -0500

    fix

--- src/components/reportSlider/MetricsReport.js
+++ src/components/reportSlider/MetricsReport.js
@@ -1,10 +1,7 @@
 import React, {useEffect, useState} from 'react'
-import List from '../List'
-import WithListLoading from '../ListLoading'
 import axios from 'axios'
 
 export default function MetricsReport() {
-  const ListLoading = WithListLoading(List)
   const [appState, setAppState] = useState({
     loading: false,
     assets: null,
@@ -20,12 +17,7 @@ export default function MetricsReport() {
   }, [setAppState]);
   return (
     <div className='MetricsReport'>
-      <div className='container'>
-        <h1>Assets</h1>
-      </div>
-      <div className='asset-container'>
-        <ListLoading isLoading={appState.loading} assets={appState.assets} />
-      </div>
+      <span>${allAssets}</span>
     </div>
   )
 }

8e0f9ec4dbb297f43f465cc47085cb0ad1b19410
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-14 02:44:23 -0500

    fix

--- src/components/reportSlider/MetricsReport.js
+++ src/components/reportSlider/MetricsReport.js
@@ -7,10 +7,6 @@ export default function MetricsReport(){
         const res = await axios.get('https://dangitsal.pythonanywhere.com/api/fullsheet')
         console.log(res.data)
 
-        return(
-          <div className='MetricsReport'>
-          </div>
-        )
     } catch (err) {
         console.error(err)
     }

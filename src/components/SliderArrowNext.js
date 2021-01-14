ea3f9e83b75247a53b2dc2a5cb8e363056578d01
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-14 14:55:31 -0500

    fix

---
+++ src/components/SliderArrowNext.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {FaCaretRight} from 'react-icons/fa'
+
+export default function SliderArrowNext(){
+    return(
+        <FaCaretRight className='arrowNext' size='32px'/>
+    )
+}
---
+++ src/components/SliderArrowPrev.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {FaCaretLeft} from 'react-icons/fa'
+
+export default function SliderArrowPrev(){
+    return(
+        <FaCaretLeft className='arrowPrev' size='32px'/>
+    )
+}

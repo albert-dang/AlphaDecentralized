68711a56c77698440f99b2f60cae189ab989e603
Albert Dang <dangitsal@users.noreply.github.com>
2021-01-22 17:16:40 -0500

    fix

--- src/components/SliderArrowNext.js
+++ src/components/SliderArrowNext.js
@@ -3,7 +3,7 @@ import {FaCaretRight} from 'react-icons/fa'
 
 export default function SliderArrowNext(){
     return(
-        <g transform="translate(20,2.5)">
+        <g transform="translate(500,2.5)">
             <FaCaretRight className='arrowNext' size='32px' style={{fill: '#423baa'}}/>
         </g>
         
--- src/components/SliderArrowPrev.js
+++ src/components/SliderArrowPrev.js
@@ -3,7 +3,7 @@ import {FaCaretLeft} from 'react-icons/fa'
 
 export default function SliderArrowPrev(){
     return(
-        <g transform="translate(20,2.5)">
+        <g transform="translate(160px)">
             <FaCaretLeft className='arrowPrev' size='32px' style={{fill: '#423baa'}}/>
         </g>
     )

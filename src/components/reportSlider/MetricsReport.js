d5e62c0cd91304cb3d8ca54700734373c9cd3d26
Albert Dang <dangitsal@users.noreply.github.com>
2021-02-05 19:54:56 -0500

    fix

--- src/components/reportSlider/MetricsReport.js
+++ src/components/reportSlider/MetricsReport.js
@@ -7,7 +7,23 @@ const Metrics = () => {
 
   return (
     <div style={{marginTop: '30px'}}>
-        {report.data['1y Target Est']}
+      1Y Target Est (Analysts): {report.data['1y Target Est']}
+      Price: {report.data['Quote Price']}
+      Ask: {report.data['Ask']}
+      Bid: {report.data['Bid']}
+      Open: {report.data['Open']}
+      Previous Close: {report.data['Previous Close']}
+      Day's Range: {report.data["Day's Range"]}
+      52 Week Range: {report.data["52 Week Range"]}
+      Beta (5Y Monthly): {report.data['Beta (5Y Monthly)']}
+      Avg Volume: {report.data['Avg. Volume']}
+      Volume: {report.data['Volume']}
+      Market Cap: {report.data['Market Cap']}
+      Earnings Date: {report.data['Earnings Date']}
+      Ex-Dividend Date: {report.data['Ex-Divident Date']}
+      Forward Divident & Yield: {report.data['Forward Divident & Yield']}
+      Earnings Per Share (1Y Trailing): {report.data['EPS (TTM)']}
+      Price/Earnings Ratio (1Y Trailing): {report.data['PE Ratio (TTM)']}
     </div>
   )
 }

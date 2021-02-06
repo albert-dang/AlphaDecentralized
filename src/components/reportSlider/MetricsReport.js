f30cdd64892e91e1a7f4879633df5de7a001c5a8
Albert Dang <dangitsal@users.noreply.github.com>
2021-02-05 20:29:20 -0500

    fix

--- src/components/reportSlider/MetricsReport.js
+++ src/components/reportSlider/MetricsReport.js
@@ -7,23 +7,23 @@ const Metrics = () => {
 
   return (
     <div style={{marginTop: '30px'}}>
-      <p>1Y Target Est (Analysts): {report.data['1y Target Est']}</p>
-      <p>Price: {report.data['Quote Price']}</p>
-      <p>Ask: {report.data['Ask']}</p>
-      <p>Bid: {report.data['Bid']}</p>
-      <p>Open: {report.data['Open']}</p>
-      <p>Previous Close: {report.data['Previous Close']}</p>
-      <p>Day's Range: {report.data["Day's Range"]}</p>
-      <p>52 Week Range: {report.data["52 Week Range"]}</p>
-      <p>Beta (5Y Monthly): {report.data['Beta (5Y Monthly)']}</p>
-      <p>Avg Volume: {report.data['Avg. Volume']}</p>
-      <p>Volume: {report.data['Volume']}</p>
-      <p>Market Cap: {report.data['Market Cap']}</p>
-      <p>Earnings Date: {report.data['Earnings Date']}</p>
-      <p>Ex-Dividend Date: {report.data['Ex-Divident Date']}</p>
-      <p>Forward Divident & Yield: {report.data['Forward Divident & Yield']}</p>
-      <p>Earnings Per Share (1Y Trailing): {report.data['EPS (TTM)']}</p>
-      <p>Price/Earnings Ratio (1Y Trailing): {report.data['PE Ratio (TTM)']}</p>
+      <p>1Y Target Est (Analysts):&nbsp;&nbsp;&nbsp;&nbsp;{report.data['1y Target Est']}</p>
+      <p>Price:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Quote Price']}</p>
+      <p>Ask:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Ask']}</p>
+      <p>Bid:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Bid']}</p>
+      <p>Open:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Open']}</p>
+      <p>Previous Close:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Previous Close']}</p>
+      <p>Day's Range:&nbsp;&nbsp;&nbsp;&nbsp;{report.data["Day's Range"]}</p>
+      <p>52 Week Range:&nbsp;&nbsp;&nbsp;&nbsp;{report.data["52 Week Range"]}</p>
+      <p>Beta (5Y Monthly):&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Beta (5Y Monthly)']}</p>
+      <p>Avg Volume:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Avg. Volume']}</p>
+      <p>Volume:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Volume']}</p>
+      <p>Market Cap:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Market Cap']}</p>
+      <p>Earnings Date:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Earnings Date']}</p>
+      <p>Ex-Dividend Date:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Ex-Divident Date']}</p>
+      <p>Forward Divident & Yield:&nbsp;&nbsp;&nbsp;&nbsp;{report.data['Forward Divident & Yield']}</p>
+      <p>Earnings Per Share (1Y Trailing):&nbsp;&nbsp;&nbsp;&nbsp;{report.data['EPS (TTM)']}</p>
+      <p>Price/Earnings Ratio (1Y Trailing):&nbsp;&nbsp;&nbsp;&nbsp;{report.data['PE Ratio (TTM)']}</p>
     </div>
   )
 }

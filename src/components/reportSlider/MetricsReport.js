a9b336b05eb0b776a6cedb7cd3acc2b288806da4
Albert Dang <dangitsal@users.noreply.github.com>
2021-02-05 22:42:38 -0500

    fix

--- src/components/reportSlider/MetricsReport.js
+++ src/components/reportSlider/MetricsReport.js
@@ -35,16 +35,16 @@ const Metrics = () => {
         <p>{report.data['Open']}</p>
         <p>{report.data['Previous Close']}</p>
         <p>{report.data["Day's Range"]}</p>
-        <p>{report.data["52 Week Range"]}</p>
+        <p>{report.data['52 Week Range']}</p>
         <p>{report.data['Beta (5Y Monthly)']}</p>
         <p>{report.data['Avg. Volume']}</p>
         <p>{report.data['Volume']}</p>
         <p>{report.data['Market Cap']}</p>
         <p>{report.data['Earnings Date']}</p>
-        <p>{report.data['Ex-Divident Date']}</p>
-        <p>{report.data['Forward Divident & Yield']}</p>
-        <p>{report.data["EPS (TTM)"]}</p>
-        <p>{report.data["PE Ratio (TTM)"]}</p>
+        <p>{report.data['Ex-Dividend Date']}</p>
+        <p>{report.data['Forward Dividend & Yield']}</p>
+        <p>{report.data['EPS (TTM)']}</p>
+        <p>{report.data['PE Ratio (TTM)']}</p>
       </div>
     </div>
   )

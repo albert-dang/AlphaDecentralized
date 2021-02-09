0281f2f216606e6dc765df9a58fb329b9b2dbbc6
Albert Dang <dangitsal@users.noreply.github.com>
2021-02-09 13:22:49 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Navbar from './components/Navbar'
 import React from 'react'
 import QueryForm from './components/queryForm/QueryForm'
 import ReportSlider from './components/reportSlider/ReportSlider'
@@ -8,6 +9,7 @@ export default function App(){
   return (
     <div className='appWrapper'>
       <>
+        <Navbar />
         <QueryForm />
         <ReportSlider />
         <ShowcaseSlider />
--- src/components/reportSlider/MetricsReport.js
+++ src/components/reportSlider/MetricsReport.js
@@ -2,7 +2,6 @@ import React, {useContext} from 'react'
 import ReportContext from '../ReportContext'
 import './MetricsReport.css'
 
-// This component displays name from Context
 const Metrics = () => {
   const report = useContext(ReportContext)
 

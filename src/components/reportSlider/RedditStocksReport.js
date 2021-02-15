ad2231995cb85f746f53447d02be8f42c075a9e0
Albert Dang <dangitsal@users.noreply.github.com>
2021-02-15 15:39:24 -0500

    fix

--- src/components/reportSlider/RedditStocksReport.js
+++ src/components/reportSlider/RedditStocksReport.js
@@ -9,27 +9,27 @@ const RedditStocks = () => {
     <div className = 'redditStocksBody' style={{marginTop: '30px'}}>
       <div className = 'tickersColumn'>      
         <p>1: {JSON.stringify(report.data)}</p>
-        <p>2: {report.data['1']}</p>
-        <p>3: {report.data['2']}</p>
-        <p>4: {report.data['3']}</p>
-        <p>5: {report.data['4']}</p>
-        <p>6: {report.data['5']}</p>
-        <p>7: {report.data['6']}</p>
-        <p>8: {report.data['7']}</p>
-        <p>9: {report.data['8']}</p>
-        <p>10: {report.data['9']}</p>
+        <p>2: {report.data}</p>
+        <p>3: {report.data}</p>
+        <p>4: {report.data}</p>
+        <p>5: {report.data}</p>
+        <p>6: {report.data}</p>
+        <p>7: {report.data}</p>
+        <p>8: {report.data}</p>
+        <p>9: {report.data}</p>
+        <p>10: {report.data}</p>
       </div>
       <div className = 'mentionsColumn'>
-        <p>{report.data['0']}</p>
-        <p>{report.data['1']}</p>
-        <p>{report.data['2']}</p>
-        <p>{report.data['3']}</p>
-        <p>{report.data['4']}</p>
-        <p>{report.data['5']}</p>
-        <p>{report.data['6']}</p>
-        <p>{report.data['7']}</p>
-        <p>{report.data['8']}</p>
-        <p>{report.data['9']}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
+        <p>{report.data}</p>
       </div>
     </div>
   )

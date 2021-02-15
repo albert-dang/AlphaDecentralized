3f7d11028feffdb48926c4b0c3d59cbefb47006e
Albert Dang <dangitsal@users.noreply.github.com>
2021-02-15 16:35:48 -0500

    fix

--- src/components/reportSlider/RedditStocksReport.js
+++ src/components/reportSlider/RedditStocksReport.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import './RedditStocksReport.css'
 
+var objToday = new Date(),
+	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+	dayOfWeek = weekday[objToday.getDay()],
+	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+	curMonth = months[objToday.getMonth()],
+	curYear = objToday.getFullYear()
+var today = '9AM ' + dayOfWeek + ' ' + dayOfMonth + ' of ' + curMonth + ', ' + curYear;
+
 var redditStocks
 
 fetch(`https://dangitsal.pythonanywhere.com/api/redditstocks`)
@@ -16,7 +26,7 @@ console.error(error)
 export default function RedditStocksReport(){
   return (
     <div className = 'redditStocksBody' style={{marginTop: '30px'}}>
-      <p>{new Date().format('m-d-Y')}</p>
+      <p>{today}</p>
       <div className = 'tickersColumn'>
         <p>1: {JSON.stringify(redditStocks)}</p>
         <p>2: {redditStocks}</p>

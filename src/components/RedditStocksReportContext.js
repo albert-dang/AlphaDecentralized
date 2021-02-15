9a2e88366c7c238f3aa2431d62095af9cb537e8a
Albert Dang <dangitsal@users.noreply.github.com>
2021-02-15 15:16:15 -0500

    fix

--- src/components/RedditStocksReportContext.js
+++ src/components/RedditStocksReportContext.js
@@ -1,9 +1,12 @@
 import React, {Component, createContext} from "react"
 
+const redditstocks
+
 fetch(`https://dangitsal.pythonanywhere.com/api/redditstocks`)
 .then(response => response.json())
 .then((jsonData) => {
     console.log(JSON.stringify(jsonData))
+    redditstocks = jsonData
 })
 .catch((error) => {
 console.error(error)
@@ -13,7 +16,7 @@ const RedditStocksReportContext = createContext()
 
 export class RedditStocksReportProvider extends Component{
   state = {
-    data: {jsonData}
+    data: redditstocks
   }
 
   setData = (data) => {

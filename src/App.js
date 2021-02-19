d3ac6327cbe60ab42908c6655f8f00c7c813ef2b
Albert Dang <dangitsal@users.noreply.github.com>
2021-02-19 01:54:00 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Navbar from './components/Navbar'
 import React from 'react'
 import QueryForm from './components/queryForm/QueryForm'
 import ReportSlider from './components/reportSlider/ReportSlider'
@@ -9,7 +8,6 @@ export default function App(){
   return (
     <div className='appWrapper'>
       <>
-        <Navbar />
         <QueryForm />
         <ReportSlider />
         <ShowcaseSlider />

f8948985565e33d3a85c3191a22fafdcfa13fa2f
Albert Dang <dangitsal@users.noreply.github.com>
2021-06-14 16:21:48 -0400

    fix

--- src/components/reportSlider/About.js
+++ src/components/reportSlider/About.js
@@ -2,11 +2,11 @@ export default function About(){
     return(
         <div className='aboutWrapper'>
             <h3>Finding alpha.</h3>
-            <p>Truly free with no user data mining. Open source and transparent.</p>
+            <p><a href='https://github.com/dangitsal/AlphaDecentralized'>Open source.</a></p>
             <br/>
-            <p>For business inquiries, collaboration or feedback, please email <a href='mailto:al@alphadecentralized.com'>al@alphadecentralized.com</a></p>
+            <p>For business inquiries, collaboration or feedback, please email <a href='mailto:contact@albertdang.tech'>contact@albertdang.tech</a></p>
             <br/>
-            <p>This is a one-man project which started in January 2021 as an effort to build financial research tools while self-teaching full-stack development from scratch.</p>
+            <p>This was a one-person project built within the month of January 2021 as an effort to build financial research tools while self-teaching full-stack development from scratch.</p>
         </div>
     )
 }

cfe0a1a7a26cbde672feb578856b5bbbb9300dba
Albert Dang <dangitsal@users.noreply.github.com>
2021-06-21 11:48:37 -0400

    fix

--- src/components/reportSlider/About.js
+++ src/components/reportSlider/About.js
@@ -4,7 +4,7 @@ export default function About(){
             <h3>Finding alpha.</h3>
             <p><a href='https://github.com/dangitsal/AlphaDecentralized'>Open source.</a></p>
             <br/>
-            <p>For business inquiries, collaboration or feedback, please email <a href='mailto:contact@albertdang.tech'>contact@albertdang.tech</a></p>
+            <p>For business inquiries, collaboration or feedback, please email <a href='mailto:hello@albertdang.ca'>hello@albertdang.ca</a></p>
             <br/>
             <p>This was a one-person project built within the month of January 2021 as an effort to build financial research tools while self-teaching full-stack development from scratch.</p>
         </div>
